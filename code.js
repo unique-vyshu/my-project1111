5f875e876284d1839e488cc5ca69b0bda7faaba5
unique-vyshu <74426792+unique-vyshu@users.noreply.github.com>
2021-08-18 17:57:53 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,81 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var boundary1 = createSprite(190, 120,420,3);
+boundary1.shapeColor="black";
+
+var boundary2 = createSprite(190,290,420,3);
+
+boundary2.shapeColor="black";
+
+var sam =createSprite(20,190,15,15);
+sam.shapeColor="green";
+var obstacle1 = createSprite(100, 150,15,15);
+obstacle1.shapeColor="red";
+var obstacle2 = createSprite(160, 280,15,15);
+obstacle2.shapeColor="red";
+var obstacle3 = createSprite(240, 150,15,15);
+obstacle3.shapeColor="red";
+var obstacle4 = createSprite(310, 280,15,15);
+
+obstacle4.shapeColor="red";
+
+
+
+
+function draw() {
+  drawSprites();
+  
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
